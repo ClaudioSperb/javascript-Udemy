7e609e9181909743aab35144968a0ecf5ec0dbd4
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-21 15:02:27 -0300

    Create index.js

---
+++ Aula 3 - Console-log/index.js
@@ -0,0 +1,9 @@
+console.log('Claudio Sperb Paulo') // usando aspas simples
+console.log("Claudio 'Sperb' Paulo") // usando aspas duplas
+console.log(`Claudio "Sperb" Paulo`) // usando crase e aspas duplas
+console.log(`Claudio 'Sperb' Paulo`) // usando crase e aspas simples
+
+// Sempre que usarmos aspas, aspas duplas ou crase, devemos abrir e fechar da mesma forma. Tem o nome de string.
+
+console.log(123456) // número
+console.log(123456, 1.5, 'Claudio Sperb')//Aqui utilizamos Números reais, inteiros e String
