551c254a6038087ea702034c4bf50ce163477499
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-22 11:48:37 -0300

    Update variaveis.js

--- Seção 2/Aula 6/variaveis.js
+++ Seção 2/Aula 6/variaveis.js
@@ -8,3 +8,16 @@ console.log('Em 2011 ele conheceu a',nomef);
 console.log('Em 2015', nomeh,  'e', nomef,'se casaram');
 console.log('Em 2023 nasceu a',nomeb, 'filha de', nomeh, 'e', nomef,'.');
 console.log(nomeb, 'nasceu dia 01 de fvereiro as 19:30 em', cidade);
+
+/*
+Regras de nomenclatura
+Caracteres permitidos: Letras (a-z, A-Z), números (0-9), sublinhado (_) e cifrão ($).
+
+Primeiro caractere: Deve começar com uma letra, sublinhado (_) ou cifrão ($). Não pode começar com um número.
+
+Sem espaços: Nomes de variáveis não podem conter espaços em branco.
+Palavras reservadas: Não podem ser usadas palavras-chave reservadas do JavaScript (como var, let, const, if, for, while, etc.).
+
+Diferenciação de maiúsculas e minúsculas: JavaScript diferencia entre maiúsculas e minúsculas (case-sensitive)
+
+*/
