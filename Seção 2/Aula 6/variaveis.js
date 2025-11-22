86faedd7df34a41022a3dfd1f10e0cdace7a494a
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-22 10:13:37 -0300

    Create variaveis.js

---
+++ Seção 2/Aula 6/variaveis.js
@@ -0,0 +1,10 @@
+let nomeh = 'Claudio'; //string
+let nomef = 'Josiane';
+let nomeb = 'Briana';
+let cidade = 'Sapucaia do Sul';
+
+console.log(nomeh,'nasceu em 1990');
+console.log('Em 2011 ele conheceu a',nomef);
+console.log('Em 2015', nomeh,  'e', nomef,'se casaram');
+console.log('Em 2023 nasceu a',nomeb, 'filha de', nomeh, 'e', nomef,'.');
+console.log(nomeb, 'nasceu dia 01 de fvereiro as 19:30 em', cidade);
