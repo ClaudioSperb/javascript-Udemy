b42fead65925a0f68943b243521378564788aad9
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-25 17:31:56 -0300

    Create index.js

---
+++ Seção 2/Aula 9 - let vs var - Primeira Diferença/index.js
@@ -0,0 +1,10 @@
+let nome = 'Claudio'
+let saudação = `Ola, boa tarde! meu nome é ${nome}, muito prazer!`
+const genero = 'Masculino'
+const cidade = 'Sapucaia do sul'
+const anoNascimento = 1990
+const idade = 35
+let anoAtual = (anoNascimento + idade)
+
+console.log(saudação)
+console.log(`Atualmente moro em ${cidade} e sou do genero ${genero}. Hoje estamos no ano de ${anoAtual} !`)
