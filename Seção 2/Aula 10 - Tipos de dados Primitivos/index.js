e46b87f4b062ce9a42f81188c254f21cb206c7da
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-26 12:56:42 -0300

    Create index.js

---
+++ Seção 2/Aula 10 - Tipos de dados Primitivos/index.js
@@ -0,0 +1,18 @@
+const nome = 'Claudio' // string
+const sobreNome = "Sperb" // string
+const sobreNomeFinal = `Paulo` // string
+
+const num = 1 // Number
+const numFlutuante = 1.5 // Number
+
+const aprovado = true // Boolean ( Lógico )
+const reprovado = false // Boolean ( Lógico )
+
+let nomeCliente // Undefined -> Não aponta pra local nenhum da memória
+let nomeClienteFinal = null // Null - Object -> Não aponta pra local nenhum da memória
+
+console.log(typeof num, num)
+console.log(typeof nome, nome)
+console.log(typeof aprovado, aprovado)
+console.log(typeof nomeCliente, nomeCliente)
+console.log(typeof nomeClienteFinal, nomeClienteFinal)
