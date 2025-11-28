60dc8d8659c95f168a4ec16446344e8cd38a8bf3
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-28 06:53:19 -0300

    Create index.js

---
+++ Seção 2/Aula 11 - Operadores Aritm., atrbuição e incremento/index.js
@@ -0,0 +1,13 @@
+const num1 = 10
+const num2 = 5
+let res = num1 + num2
+console.log(res)
+console.log('-------------------------------')
+
+res = num1 + num2 * num2
+console.log(res)
+console.log('-------------------------------')
+
+res = num1 * num2**2
+console.log(res)
+console.log('-------------------------------')
