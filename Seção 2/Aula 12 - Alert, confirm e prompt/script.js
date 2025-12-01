46ac3ea90aa52d7a1598bb1c2c8322b4e88012fa
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-12-01 16:50:33 -0300

    Update script.js

--- Seção 2/Aula 12 - Alert, confirm e prompt/script.js
+++ Seção 2/Aula 12 - Alert, confirm e prompt/script.js
@@ -3,4 +3,5 @@ let nome = window.document = prompt('Qual seu nome ?')
 const num1 = window.document = prompt(`Olá ${nome} Digite um numero: `)
 const num2 = window.document = prompt(`Certo ${nome}, Agora digite outro número: `)
 let res = Number(num1) + Number(num2)
-alert(`Você digitou ${num1} e ${num2}, a soma dos números digitados é ${res}`)
+alert(`Você digitou ${num1} e ${num2} - a soma dos números digitados é ${res}`)
+
