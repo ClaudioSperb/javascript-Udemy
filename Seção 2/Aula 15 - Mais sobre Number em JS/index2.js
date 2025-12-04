f121c0c2ed964023c15658f57193f05be1aef979
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-12-04 16:20:08 -0300

    Create index2.js

---
+++ Seção 2/Aula 15 - Mais sobre Number em JS/index2.js
@@ -0,0 +1,14 @@
+let num1 = 0.1
+let num2 = 0.7
+
+num1 += num2
+num1 += num2
+num1 += num2
+num1 += num2
+console.log(num1)
+
+
+//num1 = Number(num1.toFixed(2))
+//num2 = Number(num2.toFixed(2))
+//let res = num1 + num2
+console.log(num1)
