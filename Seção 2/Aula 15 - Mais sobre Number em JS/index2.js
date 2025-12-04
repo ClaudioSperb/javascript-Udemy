e0d4aa390356b6a4449eced87ec8610d549accd6
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-12-04 17:08:00 -0300

    Update index2.js

--- Seção 2/Aula 15 - Mais sobre Number em JS/index2.js
+++ Seção 2/Aula 15 - Mais sobre Number em JS/index2.js
@@ -8,7 +8,12 @@ num1 += num2
 console.log(num1)
 
 
-//num1 = Number(num1.toFixed(2))
-//num2 = Number(num2.toFixed(2))
-//let res = num1 + num2
+num1 = Number(num1.toFixed(2))
+num2 = Number(num2.toFixed(2))
+//Aqui usei Number com toFixed para termos um numero mais real.
 console.log(num1)
+console.log(typeof num1)
+num1 = num1.toString(2)
+console.log(num1)
+
+// Nesse caso concatenamos o valor convertido pra String com o Number.
