d6d2eaa445a82c323c088a2d9458bef54cca36e5
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-12-05 15:34:11 -0300

    Create match.js

---
+++ Seção 2/Aula 15 - Mais sobre Number em JS/match.js
@@ -0,0 +1,17 @@
+let num1 = 9.49
+let num2 = 9.54
+
+let teste = Math.floor(num1)//Aqui nesse exemplo arrendomos para baixo
+
+let teste2 = Math.ceil(num2)//Aqui nesse exemplo arredondamos para Cima
+
+console.log(teste)
+console.log(teste2)
+
+num1 = Math.round(num1)//é um método do objeto Math que arredonda um número para o inteiro mais próximo, seguindo a regra: se a parte decimal for 0.5 ou maior, arredonda para cima; se for menor que 0.5, arredonda para baixo. Ele é uma ferramenta essencial para manipulação numérica, retornando um valor inteiro. 
+
+num1 = Math.round(num2)
+//é um método do objeto Math que arredonda um número para o inteiro mais próximo, seguindo a regra: se a parte decimal for 0.5 ou maior, arredonda para cima; se for menor que 0.5, arredonda para baixo. Ele é uma ferramenta essencial para manipulação numérica, retornando um valor inteiro. 
+
+console.log(teste)
+console.log(teste2)
