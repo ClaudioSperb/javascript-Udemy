d38fb900e51a6bfdcf2d95fdfd131014a8413b3a
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-12-05 12:29:22 -0300

    Create index3.js

---
+++ Seção 2/Aula 15 - Mais sobre Number em JS/index3.js
@@ -0,0 +1,9 @@
+let num1 = 12.5
+
+let temp = num1 + 'ola'
+console.log(Number.isNaN(temp)) // testando o NaN.
+console.log(temp)
+
+let temp2 = num1 * '2'
+console.log(Number.isNaN(temp2)) // testando o NaN. utilizando multiplicação e um numero em String
+console.log(temp2)
