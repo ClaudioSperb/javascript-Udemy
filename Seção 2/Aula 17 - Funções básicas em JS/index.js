65ee46250ab6ada1bde6524ff282bf4745480a41
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-12-11 16:04:48 -0300

    criamos a aula

---
+++ Seção 2/Aula 17 - Funções básicas em JS/index.js
@@ -0,0 +1,7 @@
+function saudacao(nome){
+    console.log(`Olá, ${nome}! Seja bem-vindo(a)!`);    
+}
+
+saudacao('Claudio Sperb'); // Chamada da função
+// saudacao('Josiane'); // Chamada da função
+//saudacao('Briana'); // Chamada da função
