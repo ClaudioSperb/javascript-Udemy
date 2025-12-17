72ea98547b4a0a8b93c14c5570eba35384759301
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-12-17 12:51:29 -0300

    aula 19

--- Seção 2/Aula 19 - Valores primitivos e valorespor referência/index.js
+++ Seção 2/Aula 19 - Valores primitivos e valorespor referência/index.js
@@ -1,3 +1,23 @@
 /*
 Primitivos (Imutáveis) - String, number, boolean, undefined, null (bigint, symbol)
 */
+
+let a = 'A';
+let b = a; // Cópia do valor
+console.log(a, b); // A A
+
+a = 'Outra coisa';
+console.log(a, b); // Outra coisa A
+
+/*
+Referência (Mutável) - Array, object, function
+*/
+let c = [1, 2, 3];
+let d = c;
+console.log(c, d); // [1, 2, 3] [1, 2, 3]
+
+c.push(4);
+console.log(c, d); // [1, 2, 3, 4] [1, 2, 3, 4]
+
+d.pop();
+console.log(c, d); // [1, 2, 3] [1, 2, 3]
