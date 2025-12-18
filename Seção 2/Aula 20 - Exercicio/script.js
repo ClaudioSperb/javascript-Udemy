1d4563b1b71728f60ffd6231bb5df870cd7bd6ef
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-12-18 17:21:41 -0300

    atualização

--- Seção 2/Aula 20 - Exercicio/script.js
+++ Seção 2/Aula 20 - Exercicio/script.js
@@ -1,1 +1,19 @@
-alert('Formulário carregado com sucesso!')
+function meuEscopo() {
+
+    const form = document.querySelector('.form');
+    const nome = document.querySelector('.nome');
+    const sobrenome = document.querySelector('.sobrenome');
+    const peso = document.querySelector('.peso');
+    const altura = document.querySelector('.altura');
+    const resultado = document.querySelector('.resultado');
+
+    function receberEventoForm(evento) {
+        evento.preventDefault();
+        console.log('form não foi enviado')
+        resultado.innerHTML += `<p>Olá ${nome.value} ${sobrenome.value}, seu peso é ${peso.value}kg e sua altura é ${altura.value}m.</p>`;
+    }
+
+    form.addEventListener('submit', receberEventoForm);
+
+};
+meuEscopo();
