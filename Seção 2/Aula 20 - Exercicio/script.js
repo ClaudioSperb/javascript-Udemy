80cdc10d99f3f26111ba6b69863d2981b8bdbcc3
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-12-19 12:31:45 -0300

    finalizamos o Exercicio

--- Seção 2/Aula 20 - Exercicio/script.js
+++ Seção 2/Aula 20 - Exercicio/script.js
@@ -1,16 +1,26 @@
 function meuEscopo() {
 
     const form = document.querySelector('.form');
-    const nome = document.querySelector('.nome');
-    const sobrenome = document.querySelector('.sobrenome');
-    const peso = document.querySelector('.peso');
-    const altura = document.querySelector('.altura');
-    const resultado = document.querySelector('.resultado');
+    const pessoas = [];
+    
 
     function receberEventoForm(evento) {
         evento.preventDefault();
-        console.log('form não foi enviado')
-        resultado.innerHTML += `<p>Olá ${nome.value} ${sobrenome.value}, seu peso é ${peso.value}kg e sua altura é ${altura.value}m.</p>`;
+        const nome = document.querySelector('.nome');
+        const sobrenome = document.querySelector('.sobrenome');
+        const peso = document.querySelector('.peso');
+        const altura = document.querySelector('.altura');
+        const resultado = document.querySelector('.resultado');
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        })
+        console.log(pessoas)
+
+        resultado.innerHTML += `<p>Olá <strong>${nome.value}</strong> <strong>${sobrenome.value}</strong>, seu peso é <strong>${peso.value}kg</strong> e sua altura é <strong>${altura.value}m</strong>.</p>`;
     }
 
     form.addEventListener('submit', receberEventoForm);
