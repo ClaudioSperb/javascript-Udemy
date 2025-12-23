79cf03a7e1d3f13f66612df78225b687eb383892
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-12-23 12:37:50 -0300

    Criamos o exercicio

--- Seção 3/Aula 22 - Operadores Lógicos/index.js
+++ Seção 3/Aula 22 - Operadores Lógicos/index.js
@@ -1,9 +1,21 @@
 // Operadores Lógicos em JavaScript
 /*
  
-&& - E (AND)
-|| - OU (OR)
-! - NÃO (NOT)
+&& - E (AND) - Esse testa se as expressões são todas True para retornar true, quando houver um false, a expressao toda retorna False.
+
+|| - OU (OR) - Esse testa uma ou a outra, sendo uma verdadeira , ja retorna True, só vai retorna False, quando todas as expressões forem falsas.
+
+! - NÃO (NOT) -inver a expressão
 
 */
 
+const usuario = 'claudiosperbpaulo@gmail.com'
+const senha = '123456'
+//const usuarioLogado = usuario === 'claudiosperbpaulo@gmail.com' && senha === '123456';
+//console.log(usuarioLogado) // Nesse exemplo retornou True, pois as duas expressões são verdadeiras
+
+// const usuarioLogado = usuario === 'claudiosperbpaulo@gmail.com' || senha === '1234566458';
+// console.log(usuarioLogado) // Nesse exemplo daria TRUE pois uma delas é verdadeira. Se fosse no &&, daria FALSE.
+
+const usuarioLogado = usuario === 'claudiosperbpaulo@gmail.com' && senha === '1234564848686';
+console.log(usuarioLogado) // Nesse exemplo retornou FALSE, pois uma das duas expressões é falso.
---
+++ Seção 3/Aula 22 - Operadores Lógicos/index2.js
@@ -0,0 +1,11 @@
+//Resumindo
+
+//AND - E 
+console.log(true && true)// TRUE
+console.log(true && false)// FALSE
+console.log(false && false) // FALSE
+
+//OR - ou
+console.log(true || true) // TRUE
+console.log(true || false) // TRUE
+console.log(false || false) // FALSE
