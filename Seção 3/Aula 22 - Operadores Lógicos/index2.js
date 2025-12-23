432a1d84df6f7ae4637a40f0776b03e8aaeaa808
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-12-23 13:11:46 -0300

    Update index2.js

--- Seção 3/Aula 22 - Operadores Lógicos/index2.js
+++ Seção 3/Aula 22 - Operadores Lógicos/index2.js
@@ -9,3 +9,7 @@ console.log(false && false) // FALSE
 console.log(true || true) // TRUE
 console.log(true || false) // TRUE
 console.log(false || false) // FALSE
+
+//NOT - NÃO
+console.log(!true) // FALSE
+console.log(!false) // TRUE
