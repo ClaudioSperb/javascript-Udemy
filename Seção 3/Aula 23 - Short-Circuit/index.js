45abe801c5f93065a0803a05bd6cafdbd6bf9de4
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-12-25 20:37:23 -0300

    Update index.js

--- Seção 3/Aula 23 - Short-Circuit/index.js
+++ Seção 3/Aula 23 - Short-Circuit/index.js
@@ -10,3 +10,16 @@ NaN
 
 //Short-Circuit com AND ( && )
 console.log('Claudio' && 'Sperb' && NaN &&'Lacerda') // retorna o NaN ( um dos FALSY )
+
+function falarOi() {
+    return 'Ola !!!'
+}
+
+const VaiDarOi = true
+
+console.log( VaiDarOi && falarOi()) // Aqui no caso ele retorna a function, pois a ultima expressao validada é true a anterior tambem.
+const a = 2
+const b = false
+const c = 'false'
+const d = true
+const e = NaN
