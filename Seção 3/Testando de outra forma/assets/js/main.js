4e66318cc23f49a671791eef6d01419a65988bc0
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-01-08 11:21:02 -0300

    atualização

--- Seção 3/Testando de outra forma/assets/js/main.js
+++ Seção 3/Testando de outra forma/assets/js/main.js
@@ -1,6 +1,8 @@
 function calculoImc(){
     const form = document.querySelector('#formulario');
     const resultado = document.querySelector('#resultado');
+    const resultadoYellow = document.querySelector('#resultado-yellow');
+    const resultadoRed = document.querySelector('#resultado-red');
 
     form.addEventListener('submit', function(e) {
         e.preventDefault();
@@ -12,7 +14,7 @@ function calculoImc(){
         imc.toFixed(2);
         console.log(imc);
 
-        resultado.innerHTML = '';
+        
 
         function validaInput(){
             if (peso.value === 0){
@@ -22,23 +24,27 @@ function calculoImc(){
             }
         }
 
-        
+        resultado.innerHTML = '';
+
         if(imc < 18){
-            resultado.innerHTML += `Seu imc é de <strong>${imc.toFixed(2)}</strong> - Você esta a baixo do peso !`
+            resultadoRed.innerHTML = `Seu imc é de <strong>${imc.toFixed(2)}</strong> - Você esta a baixo do peso !`
+            
         } else if(imc >= 18.5 && imc <= 24.9){
             resultado.innerHTML = `Seu imc é de <strong>${imc.toFixed(2)}</strong> - Você esta com o peso normal !`
         } else if(imc >= 25 && imc <= 29.9){
             resultado.innerHTML = `Seu imc é de <strong>${imc.toFixed(2)}</strong> - Você esta com sobrepeso, atenção !`
         } else if(imc >= 30 && imc <= 34.9){
-            resultado.innerHTML = `Seu imc é de <strong>${imc.toFixed(2)}</strong> - Você esta com obesidade Grau 1 !`
+            resultadoYellow.innerHTML = `Seu imc é de <strong>${imc.toFixed(2)}</strong> - Você esta com obesidade Grau 1 !`
         } else if(imc >= 35 && imc <= 39.9){
-            resultado.innerHTML = `Seu imc é de <strong>${imc.toFixed(2)}</strong> - Você esta com obesidade Grau 2 !`
+            resultadoYellow.innerHTML = `Seu imc é de <strong>${imc.toFixed(2)}</strong> - Você esta com obesidade Grau 2 !`
         } else if(imc > 40){
-            resultado.innerHTML = `Seu imc é de <strong>${imc.toFixed(2)}</strong> - Você esta com obesidade Grau 3 !`
+            resultadoRed.innerHTML = `Seu imc é de <strong>${imc.toFixed(2)}</strong> - Você esta com obesidade Grau 3 !`
         } else{
             alert('Dados incorretos, tente novamente colocando somente numeros válidos !')
         }
 
     });
+
+    
 }
 calculoImc();
