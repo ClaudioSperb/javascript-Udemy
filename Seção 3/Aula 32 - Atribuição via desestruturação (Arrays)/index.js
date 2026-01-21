202f96f8abd4eccc374184491af667e795dfa965
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-01-21 20:49:58 -0300

    Update index.js

--- Seção 3/Aula 32 - Atribuição via desestruturação (Arrays)/index.js
+++ Seção 3/Aula 32 - Atribuição via desestruturação (Arrays)/index.js
@@ -1,5 +1,5 @@
 const numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
-const [primeiroNumero, segundoNumero, terceiroNumero, ...resto] = numeros;
-console.log(primeiroNumero, segundoNumero, terceiroNumero);
+const [primeiroNumero, segundoNumero, terceiroNumero, quartoNumero, ...resto] = numeros;
+console.log(primeiroNumero, segundoNumero, terceiroNumero, quartoNumero);
 console.log(resto);
