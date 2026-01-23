c1cef4c4435608d44da95925d49f1d008adf2a6b
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-01-23 14:34:21 -0300

    Atualizamos o Ex

---
+++ Seção 3/Aula 34 - For classico - Estrutura de Repetição/index.js
@@ -0,0 +1,9 @@
+// for (let i = 0; i <= 100; i ++){
+//     console.log(`Linha ${i}`)
+// }
+
+const frutas = ['Laranja', 'Maçã', 'Limão', 'Mamão'];
+
+for (let i = 0; i < frutas.length; i++){
+    console.log(`Índice ${i}`, frutas[i]);
+}
