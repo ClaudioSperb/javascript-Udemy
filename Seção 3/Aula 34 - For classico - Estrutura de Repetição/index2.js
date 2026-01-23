313d6027e32098200c8d2bce275544a710a2c03c
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-01-23 16:08:57 -0300

    Update index2.js

--- Seção 3/Aula 34 - For classico - Estrutura de Repetição/index2.js
+++ Seção 3/Aula 34 - For classico - Estrutura de Repetição/index2.js
@@ -1,3 +1,18 @@
-for(i = 0; i <= 50; i++){
-    console.log(`Linha ${i}`)
+// for(i = -50; i <= 50; i+=10){
+//     console.log(`Linha ${i}`)
+// }
+
+// console.log('*******************************')
+
+// for(i = 500; i >=400; i -= 10){
+//     console.log(`linha ${i}`)
+// }
+
+// console.log('*******************************')
+
+
+//Checando se é par ou ímpar
+for(i = 0; i <=50; i++){
+    const consulta = i % 2 === 0 ? 'é Par' : 'é Ímpar';
+    console.log(`o número ${i}`, consulta);
 }
