17d62d81cdad94946ab23c8259894e56eee74614
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-01-24 08:33:41 -0300

    Criamos o exercicio

--- Seção 3/Aula 34 - For classico - Estrutura de Repetição/index.js
+++ Seção 3/Aula 34 - For classico - Estrutura de Repetição/index.js
@@ -2,7 +2,7 @@
 //     console.log(`Linha ${i}`)
 // }
 
-const frutas = ['Laranja', 'Maçã', 'Limão', 'Mamão'];
+const frutas = ['Laranja', 'Maçã', 'Limão', 'Mamão', 'Abacaxi', 'Pêssego'];
 
 for (let i = 0; i < frutas.length; i++){
     console.log(`Índice ${i}`, frutas[i]);
---
+++ Seção 3/Aula 35 - Exercicio com for/modelo/assets/js/main.js
@@ -0,0 +1,8 @@
+const elementos = [
+    {tag: 'p', texto: 'Páragrafo criado com sucesso!'},
+    {tag: 'div', texto: 'Div criada com sucesso!'},
+    {tag: 'section', texto: 'Section criada com sucesso!'},
+    {tag: 'footer', texto: 'Footer criado com sucesso!'},
+];
+
+const container = document.querySelector('')
