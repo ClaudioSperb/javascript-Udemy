3b5139b875ab8f8bc918daabab9bf7b5270dfbe9
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-01-26 13:16:01 -0300

    Atualizamos o Ex

--- Seção 3/Aula 35 - Exercicio com for/modelo/assets/js/main.js
+++ Seção 3/Aula 35 - Exercicio com for/modelo/assets/js/main.js
@@ -1,8 +1,18 @@
 const elementos = [
-    {tag: 'p', texto: 'Páragrafo criado com sucesso!'},
-    {tag: 'div', texto: 'Div criada com sucesso!'},
-    {tag: 'section', texto: 'Section criada com sucesso!'},
-    {tag: 'footer', texto: 'Footer criado com sucesso!'},
+    {tag: 'p', texto: 'Páragrafo criado com sucesso!'}, // índice 0
+    {tag: 'div', texto: 'Div criada com sucesso!'}, // índice 1
+    {tag: 'section', texto: 'Section criada com sucesso!'}, //índice  2
+    {tag: 'footer', texto: 'Footer criado com sucesso!'}, // índice 3
 ];
 
-const container = document.querySelector('')
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+for(i = 0; i < elementos.length; i++){
+    console.log(elementos[0])
+}
+
+
+
+console.log(elementos);
+
